27beb6ab576505d16c33f0f0c1f3ca8f76cd7810
dahoas <alexdahoas@gmail.com>
2021-11-15 10:21:33 -0500

    test

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 if __name__ == "__main__":
     m = main.Model()
     trainer = pl.Trainer(
-        gpus=1,
+        gpus=0,
         accumulate_grad_batches=8,
         checkpoint_callback=ModelCheckpoint(),)
     trainer.fit(m)
